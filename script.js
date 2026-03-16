dd22db5b4bb51a0b0209e5a6d66e2970d24a6959
cesarsanchezmoreno599-collab <cesarsanchezmoreno599@gmail.com>
2026-03-16 03:47:20 -0600

    Actualización 0.4.3

--- script.js
+++ script.js
@@ -1,91 +1,22 @@
-/* ELEMENTOS */
-
-const modal = document.getElementById("modalFormulario");
-const abrir = document.getElementById("abrirModal");
-const cerrar = document.querySelector(".cerrar");
-
-/* ABRIR MODAL */
-
-if (abrir && modal) {
-
-abrir.addEventListener("click", () => {
-
-modal.style.display = "flex";
-
-setTimeout(()=>{
-modal.classList.add("activo");
-},10);
-
-});
-
-}
-
-/* CERRAR MODAL */
-
-function cerrarModal(){
-
-modal.classList.remove("activo");
-
-setTimeout(()=>{
-modal.style.display = "none";
-},200);
-
-}
-
-/* CERRAR CON X */
-
-if(cerrar){
-
-cerrar.addEventListener("click", cerrarModal);
-
-}
-
-/* CERRAR AL HACER CLICK FUERA */
-
-window.addEventListener("click",(e)=>{
-
-if(e.target === modal){
-
-cerrarModal();
-
-}
-
-});
-
-/* CERRAR CON ESC */
-
-document.addEventListener("keydown",(e)=>{
-
-if(e.key === "Escape" && modal.style.display === "flex"){
-
-cerrarModal();
-
-}
-
-});
-
-/* ANIMACION SCROLL */
-
 const reveals = document.querySelectorAll(".reveal");
 
 function revealScroll(){
 
-for(let i=0;i<reveals.length;i++){
-
 const windowHeight = window.innerHeight;
 
-const elementTop = reveals[i].getBoundingClientRect().top;
+reveals.forEach(el=>{
 
-const visible = 120;
+const elementTop = el.getBoundingClientRect().top;
 
-if(elementTop < windowHeight - visible){
+if(elementTop < windowHeight - 120){
 
-reveals[i].classList.add("active");
-
-}
+el.classList.add("active");
 
 }
+
+});
 
 }
 
 window.addEventListener("scroll", revealScroll);
+window.addEventListener("load", revealScroll);
